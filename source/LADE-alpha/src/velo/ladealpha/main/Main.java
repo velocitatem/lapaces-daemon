bd69a21f05d8eb77b203a0f7324dee919970465a
Daniel Rosel <danalves24@outlook.com>
2021-10-16 14:25:37 +0200

    basic-cli-interface

--- source/LADE-alpha/src/velo/ladealpha/main/Main.java
+++ source/LADE-alpha/src/velo/ladealpha/main/Main.java
@@ -1,11 +1,62 @@
 package velo.ladealpha.main;
 
+import java.util.Scanner;
+
 import velo.ladealpha.fields.astronomy.celestial_systems.SystemAlpha;
 
 public class Main {
 
 	public static void main(String[] args) {
-		SystemAlpha sysA = new SystemAlpha();
-		System.out.println(sysA.radiansOfDifferenceAfterNDays(165));
+		System.out.println(
+				" _        _    ____  _____\n" + 
+				"| |      / \\  |  _ \\| ____|\n" + 
+				"| |     / _ \\ | | | |  _|\n" + 
+				"| |___ / ___ \\| |_| | |___\n" + 
+				"|_____/_/   \\_|____/|_____|\n" + 
+				"LApaces DEmon - Elementary Computational Inteligence by Daniel Rosel \n"
+				);
+	
+		System.out.println("Please selct a module to load:");
+		System.out.println(				
+				"+----------------+\n" + 
+				"|    Modules     |\n" + 
+				"+----------------+\n" + 
+				"| 1) Astronomy   |\n" + 
+				"| 2) Psychology  |\n" + 
+				"| 3) Economy     |\n" + 
+				"| 4) Mathematics |\n" + 
+				"| 5) Politics    |\n" + 
+				"| 6) Physics     |\n" + 
+				"+----------------+\n" + 
+				"\n" + 
+				""
+				);
+		Scanner myObj = new Scanner(System.in);
+		System.out.print("\n > ");
+		String choice = myObj.nextLine(); 
+		
+		System.out.println("Loading module "+ choice + "...");
+		String[] moduleOptions = {				
+				"+----------------------------------------------+\n" + 
+				"|                  Astronomy                   |\n" + 
+				"+----------------------------------------------+\n" + 
+				"| 1) Radians of the moon relative to Full Moon |\n" + 
+				"| 2) N orbits of moon after D days             |\n" + 
+				"+----------------------------------------------+\n" 				
+				,
+				"\n" + 
+				"+------------------------+\n" + 
+				"|       Psychology       |\n" + 
+				"+------------------------+\n" + 
+				"| 1) Behavior Prediction |\n" + 
+				"| 2) Behavior Analysis   |\n" + 
+				"+------------------------+\n" 
+			
+		};
+		System.out.println(moduleOptions[Integer.valueOf(choice)-1]);
+		System.out.print("\n > ");
+		String choice1 = myObj.nextLine();
+		
+		
 	}
 }
