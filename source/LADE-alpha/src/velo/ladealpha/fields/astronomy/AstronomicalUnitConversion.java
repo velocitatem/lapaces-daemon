0dd3c7277e086dd4d54f07fa2d63feefa041cefd
Daniel Rosel <danalves24@outlook.com>
2021-10-24 18:02:23 +0200

    StelarDistances

---
+++ source/LADE-alpha/src/velo/ladealpha/fields/astronomy/AstronomicalUnitConversion.java
@@ -0,0 +1,7 @@
+package velo.ladealpha.fields.astronomy;
+
+public class AstronomicalUnitConversion {
+	public static double AUtoLY(double in) {
+		return in * (1.5813 * Math.pow(10, -5));
+	}
+}
---
+++ source/LADE-alpha/src/velo/ladealpha/fields/math/operations/Interpolation.java
@@ -0,0 +1,7 @@
+package velo.ladealpha.fields.math.operations;
+
+public class Interpolation {
+	public Interpolation(double[][] set, double x) {
+		
+	}
+}
