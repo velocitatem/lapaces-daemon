84cde9d25c033f5916cac5f79f3d6599970bcdc4
Daniel Rosel <danalves24@outlook.com>
2021-10-25 14:06:53 +0200

    velo 22576 -

--- source/LADE-alpha/src/velo/ladealpha/fields/math/Equation.java
+++ source/LADE-alpha/src/velo/ladealpha/fields/math/Equation.java
@@ -2,12 +2,25 @@ package velo.ladealpha.fields.math;
 
 import java.util.ArrayList;
 
+import velo.ladealpha.fields.services.Codify;
+
 /**
  * @author velo
  * 
  */
 public class Equation{
+	private String stringForm;
 	public double compute(double input) {
-		return -1;
-	}
+		if(stringForm == null) {
+			return -1;
+		}
+		else {
+			Codify c = new Codify();
+			return c.computeStringEquation(stringForm, input);
+		}
+	}	
+	public Equation equationFromString(String eq) {
+		stringForm = eq;
+		return this;
+	}	
 }
