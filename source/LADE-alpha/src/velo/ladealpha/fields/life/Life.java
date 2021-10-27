baac54fc7125fb43a6830f92656fb3bad5ff32ea
Daniel Rosel <danalves24@outlook.com>
2021-10-27 20:45:46 +0200

    velo 21549 -

--- source/LADE-alpha/src/velo/ladealpha/fields/life/Life.java
+++ source/LADE-alpha/src/velo/ladealpha/fields/life/Life.java
@@ -1,6 +1,7 @@
 package velo.ladealpha.fields.life;
 
 public class Life {
+	
 	private String subject;
 	public Behavior[] patterns;
 	
@@ -8,6 +9,16 @@ public class Life {
 		super();
 		this.subject = subject;
 		this.patterns = patterns;
+	}
+	
+	public String getFunctionSet() {
+		String r = "";
+		for(Behavior b : patterns) {
+			for(Event e : b.getEvents()) {
+				r+=e+"\n";
+			}
+		}
+		return r;
 	}
 	
 	public Event getClosestEvent(double day, double hour) {
@@ -20,17 +31,17 @@ public class Life {
 			for(Event e : b.getEvents()) {
 				double diff = Math.abs(e.closestOccurance(x) - x);
 				System.out.println(e);
-				if(diff<smallestDiff) {
-					
+				if(diff<smallestDiff) {	
 					smallestDiff = diff;
 					closestX = 2;
 					closestEvent = e;
 				}
 			}
 		}
-		System.out.println((smallestDiff/(Math.PI*2))*24);
+//		System.out.println((smallestDiff/(Math.PI*2))*24);
 		return closestEvent;
 	}
+	
 	/**
 	 * @return the subject
 	 */
--- source/LADE-alpha/src/velo/ladealpha/fields/life/testing/LifeTesting.java
+++ source/LADE-alpha/src/velo/ladealpha/fields/life/testing/LifeTesting.java
@@ -10,14 +10,29 @@ import velo.ladealpha.fields.life.Life;
 class LifeTesting {
 
 	@Test
-	void test() {
-		Life life = new Life("John Doe", new Behavior[] {						
-				new Behavior("Waking up", 1, 6.5),
-				new Behavior("Preparing for school",1, 7, 8),
-				new Behavior("Covid Test", 3, 17),
-				new Behavior("Text friends", 4, 20, 21)
+	void weekend() {
+		Life life = new Life("Everyone", new Behavior[] {
+
 		});
-		System.out.println(life.getClosestEvent(5, 18).getName());
+		Behavior b = new Behavior("Weekend Work Negation", 6, 12);
+		System.out.println(b.getEvents()[0].toString());
+	}
+
+	@Test
+	void test() {
+
+		// sample life model
+		Life life = new Life("Daniel Rosel", new Behavior[] {
+
+				new Behavior("Waking up", 1, 5.75), new Behavior("Preparing for the day", 2, 5.75, 6.25),
+				new Behavior("Covid Test", 3, 14), new Behavior("Text friends", 4, 20, 21),
+				new Behavior("Gym", 2, 6.5, 7.5), new Behavior("Homework", 1, 19, 21),
+				new Behavior("School", 1, 8.5, 15.5), new Behavior("Work", 1, 11.5, 12.25),
+				new Behavior("Work", 1, 16, 18), new Behavior("Dinner", 1, 18, 19), new Behavior("Dentist", 100, 12)
+
+		});
+
+		assertEquals("Work end", life.getClosestEvent(1, 18.2).getName());
 	}
 
 }
