f263b65b2818cbfcb5587b018d4f3ad1cad57122
Daniel Rosel <60182044+danalves24com@users.noreply.github.com>
2021-11-19 19:09:48 +0100

    Update .install_lada.sh

--- source/LADA-shell/.install_lada.sh
+++ source/LADA-shell/.install_lada.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curl -L https://github.com/danalves24com/lapaces-daemon/releases/download/v4.0.0/lade-shell-v4.jar -o ./.lada/lada.jar
 cp -r ./.lada /bin
 cp -r /bin/.lada/lada /bin/lada
 chmod +x /bin/lada
