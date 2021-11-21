db9c276762d09ea84b2b5d975c8bff4407100d65
Daniel Rosel <60182044+danalves24com@users.noreply.github.com>
2021-11-21 15:03:11 +0100

    updated LADAshell to v5 ins install script

--- source/LADA-shell/.install_lada.sh
+++ source/LADA-shell/.install_lada.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/danalves24com/lapaces-daemon/releases/download/v4.0.0/lade-shell-v4.jar -o ./.lada/lada.jar
+curl -L https://github.com/danalves24com/lapaces-daemon/releases/download/LSv5.0.01/lade-shell-v5.jar -o ./.lada/lada.jar
 cp -r ./.lada /bin
 cp -r /bin/.lada/lada /bin/lada
 chmod +x /bin/lada
