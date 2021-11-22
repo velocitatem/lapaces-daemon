e90d6dc754efd6e695b1a3fbe129f384116a5586
Daniel Rosel <60182044+danalves24com@users.noreply.github.com>
2021-11-22 11:49:08 +0100

    non-dependant installation script

--- source/LADA-shell/.install_lada.sh
+++ source/LADA-shell/.install_lada.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+mkdir .lada
+touch .lada/lada
+echo "#!/bin/bash" > .lada/lada
+echo "java -jar /bin/.lada/lada.jar" > .lada/lada
 curl -L https://github.com/danalves24com/lapaces-daemon/releases/download/LSv5.0.01/lade-shell-v5.jar -o ./.lada/lada.jar
-cp -r ./.lada /bin
-cp -r /bin/.lada/lada /bin/lada
-chmod +x /bin/lada
+cp -r ./.lada /usr/bin
+cp -r /bin/.lada/lada /usr/bin/lada
+chmod +x /usr/bin/lada
