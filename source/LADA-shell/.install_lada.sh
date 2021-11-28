0346f2959727fcaeee0e7a746beb88b8faaf3231
Daniel Rosel <60182044+danalves24com@users.noreply.github.com>
2021-11-28 10:55:01 +0100

    Update .install_lada.sh

--- source/LADA-shell/.install_lada.sh
+++ source/LADA-shell/.install_lada.sh
@@ -6,4 +6,5 @@ echo "java -jar /usr/bin/.lada/lada.jar /usr/bin/.lada/creds.json" > .lada/lada
 touch .lada/creds.json
 curl -L https://github.com/danalves24com/lapaces-daemon/releases/download/LSv5.6/lade-shell-v6.jar -o ./.lada/lada.jar
 cp -r ./.lada /usr/bin
+cp -r /usr/bin/.lada/lada /usr/bin/lada
 chmod +x /usr/bin/lada
