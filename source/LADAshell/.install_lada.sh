ded71f4d8efe46ccd0b8e0d5853d6d71817461a3
danalves24com <danalves24@outlook.com>
2021-12-03 09:37:09 +0100

    lada shell install script

---
+++ source/LADAshell/.install_lada.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir .lada
+touch .lada/lada
+echo "#!/bin/bash" > .lada/lada
+echo "java -jar /usr/bin/.lada/lada.jar /usr/bin/.lada/creds.json" > .lada/lada
+touch .lada/creds.json
+curl -L https://github.com/danalves24com/lapaces-daemon/releases/download/LSv5.6/lade-shell-v6.jar -o ./.lada/lada.jar
+cp -r ./.lada /usr/bin
+cp -r /usr/bin/.lada/lada /usr/bin/lada
+chmod +x /usr/bin/lada
